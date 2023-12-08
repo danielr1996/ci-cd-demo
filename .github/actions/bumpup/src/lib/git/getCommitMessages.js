815ec18119a7b9b840e2ae6742af9eb8f2e075e8
Daniel Richter <danielrichter@posteo.de>
2023-12-08 02:13:05 +0100

    chore(ci): correctly determine version if there is no initial tag
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- .github/actions/bumpup/src/lib/git/getCommitMessages.js
+++ .github/actions/bumpup/src/lib/git/getCommitMessages.js
@@ -1,14 +1,13 @@
 import {git, fs} from "../../deps.js";
 
 export const getCommitMessages = async (currentversion, dir)=>{
-    try{
-        const tagOid = await git.resolveRef({ fs, dir, ref: currentversion });
-        const tagCommit = await git.readCommit({ fs, dir, oid: tagOid });
         const commits = await git.log({ fs, dir });
-        const position = commits.map((c) => c.oid).indexOf(tagCommit.oid);
-        const relevantCommits = commits.slice(0, position).map((c) => c.commit)
-        return relevantCommits.map((c) => c.message);
-    }catch (_){
-        return []
-    }
+        if(currentversion !== '0.0.0'){
+                const tagOid = await git.resolveRef({ fs, dir, ref: currentversion });
+                const tagCommit = await git.readCommit({ fs, dir, oid: tagOid });
+                const position = commits.map((c) => c.oid).indexOf(tagCommit.oid);
+                const relevantCommits = commits.slice(0, position).map((c) => c.commit)
+                return relevantCommits.map((c) => c.message);
+        }
+        return commits.map((c) => c.commit).map((c) => c.message)
 }
