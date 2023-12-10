5d767d41e13bdd2f774833139110569a579524a4
Daniel Richter <danielrichter@posteo.de>
2023-12-10 20:06:57 +0100

    feat: display environment
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- backend/index.mjs
+++ backend/index.mjs
@@ -3,7 +3,10 @@ import buildmeta from "./buildmeta.json" assert { type: 'json' };
 
 const server = http.createServer((req, res) =>{
     res.writeHead(200,{'Content-Type': 'application/json','Access-Control-Allow-Origin': '*'});
-    res.end(JSON.stringify(buildmeta));
+    res.end(JSON.stringify({
+        buildmeta,
+        environment: process.env['ENVIRONMENT']
+    }));
 });
 server.listen(8080,'0.0.0.0')
 console.log('Started to listen on 0.0.0.0:8080')
