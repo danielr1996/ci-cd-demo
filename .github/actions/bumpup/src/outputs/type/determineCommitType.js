f39e3369a8e90f660d44014c234e2db0229d0a9b
Daniel Richter <danielrichter@posteo.de>
2023-12-10 20:47:16 +0100

    chore(ci): correctly determine type on feature branches
    Signed-off-by: Daniel Richter <danielrichter@posteo.de>

--- .github/actions/bumpup/src/outputs/type/determineCommitType.js
+++ .github/actions/bumpup/src/outputs/type/determineCommitType.js
@@ -1,12 +1,19 @@
 import {match} from "../../lib/match.js";
 
-export const determineCommitType = (isPre)=>(message) => match([
-    [isPre, 'prerelease'],
-    [message.header.includes("BREAKING CHANGE"),"major"],
-    [message.type === "feat","minor"],
-    [message.type === "fix","patch"],
-    [message.type === "perf","patch"],
-    [message.type === "refactor","patch"],
-    [message.type === "deps","patch"],
-    [true, "none"]
-]);
+export const determineCommitType = (isPre)=>(message) => {
+    const type = match([
+        [message.header.includes("BREAKING CHANGE"),"major"],
+        [message.type === "feat","minor"],
+        [message.type === "fix","patch"],
+        [message.type === "perf","patch"],
+        [message.type === "refactor","patch"],
+        [message.type === "deps","patch"],
+        [true, "none"]
+    ]);
+
+    return match([
+        [isPre && type !== 'none', 'prerelease'],
+        [type !== 'none', type],
+        [true, "none"]
+    ])
+}
